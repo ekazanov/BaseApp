f5a00a71cf500df5daa9d2c9df3c0aa5897d8d5c
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2019-09-08 21:38:18 +0100

    004_simple_app.py started.

---
+++ 004_simple_app.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+"""
+
+# TODO Add message handler to worker
+# TODO Add message sending to worker
+
+from __future__ import print_function
+
+__author__ = "Evgeny Kazanov"
+
+from base_app.multiprocess.main import Main
+from base_app.multiprocess.worker import Worker
+
+class UserWorker(Worker):
+
+    def __init__(self, *args, **kwargs):
+        super(UserWorker, self).__init__(*args, **kwargs)
+        self.main_loop_sleep_time = 1
+
+    def worker_action(self):
+        print("UsersWorker: {}".format(self.name))
+        return
+
+class UserMain(Main):
+
+    def main_action(self):
+        print("UserMain.main_action()")
+        return
+
+    
+    
+main = UserMain()
+main.main_loop_sleep_time = 0.5
+worker = UserWorker(name='Worker 01')
+main.register_worker(worker=worker)
+worker = UserWorker(name='Worker 02')
+main.register_worker(worker=worker)
+
+main.run()
