5414fc062f9ed80f5dad3c3fc9dd2b9d94324a04
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2019-09-20 22:01:21 +0100

    005_messages.py - two UserWorker classes.

--- 005_messages.py
+++ 005_messages.py
@@ -17,10 +17,10 @@ import sys
 from base_app.multiprocess.main import Main
 from base_app.multiprocess.worker import Worker
 
-class UserWorker(Worker):
+class UserWorker01(Worker):
 
     def __init__(self, *args, **kwargs):
-        super(UserWorker, self).__init__(*args, **kwargs)
+        super(UserWorker01, self).__init__(*args, **kwargs)
         self.main_loop_sleep_time = 1
         self.msg_receiver.register_handler(
             message_type="print",
@@ -34,6 +34,22 @@ class UserWorker(Worker):
         print("Worker: {}. Message: {}".format(self.name, msg_body))
         return
 
+class UserWorker02(Worker):
+
+    def __init__(self, *args, **kwargs):
+        super(UserWorker02, self).__init__(*args, **kwargs)
+        self.main_loop_sleep_time = 1
+        self.msg_receiver.register_handler(
+            message_type="print",
+            message_handler=self._msg_handl_print_msg)
+
+    def worker_action(self):
+        print("UsersWorker: {}".format(self.name))
+        return
+
+    def _msg_handl_print_msg(self, msg_body=None):
+        print("Worker: {}. Message: {}".format(self.name, msg_body))
+        return
 
 class UserMain(Main):
 
@@ -45,14 +61,14 @@ class UserMain(Main):
     def _send_msg_to_worker_01(self):
         self.msg_router.send_message(receiving_object_name="Worker_01",
                                      message_type="print",
-                                     message_body="Message to Worker_01")
+                                     message_body="Message from Main to Worker_01")
         return
 
 main = UserMain()
 main.main_loop_sleep_time = 0.5
-worker = UserWorker(name='Worker_01')
+worker = UserWorker01(name='Worker_01')
 main.register_worker(worker=worker)
-worker = UserWorker(name='Worker_02')
+worker = UserWorker02(name='Worker_02')
 main.register_worker(worker=worker)
 
 print("main.msg_router.message_route_d = {}".format(main.msg_router.message_route_d))
