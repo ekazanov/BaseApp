9615d14d8dee08a0351f9060a1c56003bfe5b700
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2019-10-13 22:02:14 +0100

    Add TODOS to the TaskQueue class.

--- base_app/multiprocess/task_queue.py
+++ base_app/multiprocess/task_queue.py
@@ -1,7 +1,19 @@
 """
 Project: BaseApp. Class: TaskQueue.
+
+TODO: Add a Queue attribute.
+TODO: Add a TaskQueue.get_task() method.
+TODO: Add a Worker.task_queue attribute
+TODO: Design the rest of the task_queue processing.
 """
+
 from multiprocessing import Queue
+try:
+    # Python2
+    from Queue import Empty
+except ModuleNotFoundError:
+    # Python3
+    from queue import Empty
 
 class TaskQueue(object):
     """
@@ -11,4 +23,3 @@ class TaskQueue(object):
         """
         """
         pass
-
