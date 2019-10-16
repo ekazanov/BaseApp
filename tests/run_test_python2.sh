9c12b0ea50658f4901edfccc5202d0e3e09228b7
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2019-10-16 21:33:32 +0100

    Add test running scripts.

---
+++ tests/run_test_python2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# Author Evgeny Kazanov
+
+if [[ $# -ne 1 ]]
+then
+    echo
+    echo "Usage: ./run_example_python2.sh <test file name>"
+    echo
+    exit 1
+fi
+
+CUR_DIR=`pwd`
+cd ..
+PYTHON_BASE=`pwd`
+cd $CUR_DIR
+
+PYTHONPATH=$PYTHONPATH:$PYTHON_BASE
+
+python2 $1
